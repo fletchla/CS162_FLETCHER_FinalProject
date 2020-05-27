36f84ce718c5673341808c9cc10a7be8ed8f3938
fletchla <fletchla@gmail.com>
2020-05-26 23:11:00 -0700

    added array list for zombies and humans, draws them, and draws the count of each species

--- src/WorldCreator.java
+++ src/WorldCreator.java
@@ -1,28 +1,53 @@
 import processing.core.PApplet;
 import java.util.ArrayList;
 
+
 public class WorldCreator extends PApplet {
-    Creature creature;
-    ArrayList<Creature> creatures = new ArrayList<Creature>();
+    ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+    ArrayList<Human> humans = new ArrayList<Human>();
 
     public void settings() {
         size(500,500);
     }
 
     public void setup() {
+        int humanCount = this.getInitialPopulation();
+        int zombieCount = this.getInitialPopulation();
 
-        creatures.add(new Human(new SpawnPoint(250, 400), 50.0));
-        creatures.add(new Zombie(new SpawnPoint(250, 100), 50.0));
+        for(int i = 0; i < humanCount; i++) {
+            humans.add(new Human());
+        }
 
+        for(int i = 0; i < zombieCount; i++) {
+            zombies.add(new Zombie());
+        }
     }
 
     public void draw() {
-        //you'll need to draw all your shapes
-        //this is your ShapeCreator object which is your sketch.
-        //pass shape.draw the sketch so it can draw on it
-        for(Creature shape: this.creatures){
-            shape.draw(this);
+        background(250);
+
+        for(Human human: this.humans){
+            human.draw(this);
         }
+
+        for(Zombie zombie: this.zombies){
+            zombie.draw(this);
+        }
+
+        drawCounters();
+
+    }
+
+    public int getInitialPopulation(){
+        return (int) random(10, 50);
+    }
+
+    public void drawCounters(){
+        textSize(30);
+        fill(0);
+        textAlign(TOP, CENTER);
+        text("Zombies: " + this.zombies.size(), 250, 50);
+        text("Humans: " + this.humans.size(), 250, 450);
 
     }
 }
