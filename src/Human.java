7ebfe349c2ecfcc83631bf920250e0455058fbb8
fletchla <fletchla@gmail.com>
2020-05-26 23:08:51 -0700

    added color and velocity

--- src/Human.java
+++ src/Human.java
@@ -2,26 +2,15 @@ import processing.core.PApplet;
 
 public class Human extends Creature {
 
-    private double radius;
 
-    public Human(SpawnPoint center, double radius) {
-        super(center);
-        this.radius = radius;
+    public Human() {
+        super();
+        setupSpawnPoint(450, 490);
+        setupColor(200, 255, 0, 150, 0, 150);
+        setyVelocity(-1);
+
     }
 
-    public double computeArea() {
-        return Math.PI*Math.pow(radius,2);
-    }
 
-    public double getPerimeter() {
-        return 2*Math.PI*radius;
-    }
 
-    public void draw(PApplet p){
-        p.circle(
-                (float) this.getPosition().getX(),
-                (float) this.getPosition().getY(),
-                (float) this.radius
-        );
-    }
 }
